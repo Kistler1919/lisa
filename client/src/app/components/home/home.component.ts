abd43c480dd72404a0db3b09228b389290695e57
Kistler <63625414+Kistler1919@users.noreply.github.com>
2021-01-28 23:35:16 +0100

    User Registrations

---
+++ client/src/app/components/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line: typedef
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  // tslint:disable-next-line: typedef
+  // getUsers() {
+  //   this.http.get('https://localhost:5001/api/users')
+  //     .subscribe(users => {
+  //       this.users = users;
+  //     }, error => {
+  //       console.log(error);
+  //     });
+  // }
+
+  // tslint:disable-next-line: typedef
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+
+}
